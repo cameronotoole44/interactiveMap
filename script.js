bc422c3531f23ca0c32ad97391ccade2aff42891
cameronotoole44 <cameronotoole404@gmail.com>
2024-03-23 12:34:09 -0400

    final comments and refactored code blocks

--- script.js
+++ script.js
@@ -27,15 +27,12 @@ const mainMap = {
         }
     },
 };
-
-//get coordinates geolocation 
 async function getCoordinates() {
     const position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
     return [position.coords.latitude, position.coords.longitude]
 }
-// get foursquare data 
 async function getFoursquare(business) {
     try {
         const accessToken = 'my_api_key';
@@ -49,7 +46,6 @@ async function getFoursquare(business) {
         if (!response.ok) {
             throw new Error('failed to fetch data');
         }
-
         const data = await response.json();
         return processBusinesses(data.results);
     } catch (error) {
@@ -64,36 +60,17 @@ function processBusinesses(results) {
         long: element.geocodes.main.longitude
     }));
 }
-//event listeners 
 window.onload = async () => {
     const coordinates = await getCoordinates()
-    // console.log(coordinates) // passed 
     mainMap.coordinates = coordinates
     mainMap.buildMap()
 }
-// business submit button
 document.getElementById('submit').addEventListener('click', async (event) => {
     event.preventDefault()
 
-    // try {
-    //     let business = document.getElementById('business').value;
-    //     let data = await getFoursquareData(business);
-
-    //     if (data && data.length > 0) {
-    //         mainMap.businesses = processBusinesses(data);
-    //         mainMap.addMarkers();
-    //     } else {
-    //         console.error('no data received or empty response');
-    //     }
-    // } catch (error) {
-    //     console.error('error fetching data:', error);
-    // }
-
-    // ternary operator instead of if/else 
     try {
         let business = document.getElementById('business').value;
         let data = await getFoursquare(business);
-
         data && data.length > 0
             ? (mainMap.businesses = processBusinesses(data), mainMap.addMarkers()) // checks if data is truthy and if it contains items if true the markers are added
             : console.error('no data received or empty response');
